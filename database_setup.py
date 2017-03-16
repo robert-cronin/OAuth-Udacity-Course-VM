7d9d1bc51abeb35d24a42f163bd1441d5aaacb09
Robbie Cronin <robert.cronin@uqconnect.edu.au>
2017-03-16 23:20:25 +1000

    Update database files

--- database_setup.py
+++ database_setup.py
@@ -2,14 +2,26 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    name = Column(String(80), nullable = False)
+    email = Column(String(200), nullable = False)
+    picture = Column(String(400))
+    id = Column(Integer, primary_key = True)
+
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
+
 
     @property
     def serialize(self):
@@ -18,7 +30,7 @@ class Restaurant(Base):
            'name'         : self.name,
            'id'           : self.id,
        }
- 
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
@@ -30,22 +42,21 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
 
 
     @property
     def serialize(self):
        """Return object data in easily serializeable format"""
        return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-           'price'         : self.price,
-           'course'         : self.course,
+           'name' : self.name,
+           'description' : self.description,
+           'id' : self.id,
+           'price' : self.price,
+           'course' : self.course,
        }
 
-
-
-engine = create_engine('sqlite:///restaurantmenu.db')
- 
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 
 Base.metadata.create_all(engine)
